10a9174cbc3da0c4ea846070d9351152c19d44f4
DzLyz <86110711+itsdzl@users.noreply.github.com>
2022-07-28 16:39:53 +0700

    Update vars.py

--- DzL/plugins/sudo/vars.py
+++ DzL/plugins/sudo/vars.py
@@ -13,10 +13,10 @@ from pyrogram import filters
 
 import config
 from strings import get_command
-from YukkiMusic import app
-from YukkiMusic.misc import SUDOERS
-from YukkiMusic.utils.database.memorydatabase import get_video_limit
-from YukkiMusic.utils.formatters import convert_bytes
+from DzL import app
+from DzL.misc import SUDOERS
+from DzL.utils.database.memorydatabase import get_video_limit
+from DzL.utils.formatters import convert_bytes
 
 VARS_COMMAND = get_command("VARS_COMMAND")
 
