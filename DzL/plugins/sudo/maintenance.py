256960cbed90ff377710272a7e9d1cb877422cc4
DzLyz <86110711+itsdzl@users.noreply.github.com>
2022-07-28 16:37:39 +0700

    Update maintenance.py

--- DzL/plugins/sudo/maintenance.py
+++ DzL/plugins/sudo/maintenance.py
@@ -11,12 +11,12 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from strings import get_command, get_string
-from YukkiMusic import app
-from YukkiMusic.misc import SUDOERS
-from YukkiMusic.utils.database import (get_lang, is_maintenance,
+from DzL import app
+from DzL.misc import SUDOERS
+from DzL.utils.database import (get_lang, is_maintenance,
                                        maintenance_off,
                                        maintenance_on)
-from YukkiMusic.utils.decorators.language import language
+from DzL.utils.decorators.language import language
 
 # Commands
 MAINTENANCE_COMMAND = get_command("MAINTENANCE_COMMAND")
