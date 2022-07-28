68dc62cd182381f26c819b44ee3d7bad8ce8aa4d
DzLyz <86110711+itsdzl@users.noreply.github.com>
2022-07-28 16:15:34 +0700

    Update help.py

--- DzL/plugins/bot/help.py
+++ DzL/plugins/bot/help.py
@@ -15,13 +15,13 @@ from pyrogram.types import InlineKeyboardMarkup, Message
 
 from config import BANNED_USERS
 from strings import get_command, get_string, helpers
-from YukkiMusic import app
-from YukkiMusic.misc import SUDOERS
-from YukkiMusic.utils import help_pannel
-from YukkiMusic.utils.database import get_lang, is_commanddelete_on
-from YukkiMusic.utils.decorators.language import (LanguageStart,
+from DzL import app
+from DzL.misc import SUDOERS
+from DzL.utils import help_pannel
+from DzL.utils.database import get_lang, is_commanddelete_on
+from DzL.utils.decorators.language import (LanguageStart,
                                                   languageCB)
-from YukkiMusic.utils.inline.help import (help_back_markup,
+from DzL.utils.inline.help import (help_back_markup,
                                           private_help_panel)
 
 ### Command
