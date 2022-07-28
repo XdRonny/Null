aa254b0df69ebb26074e734fe196fecf8062ecf1
DzLyz <86110711+itsdzl@users.noreply.github.com>
2022-07-28 16:13:49 +0700

    Update stop.py

--- DzL/plugins/admins/stop.py
+++ DzL/plugins/admins/stop.py
@@ -12,10 +12,10 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import get_command
-from YukkiMusic import app
-from YukkiMusic.core.call import Yukki
-from YukkiMusic.utils.database import set_loop
-from YukkiMusic.utils.decorators import AdminRightsCheck
+from DzL import app
+from DzL.core.call import Dz
+from DzL.utils.database import set_loop
+from DzL.utils.decorators import AdminRightsCheck
 
 # Commands
 STOP_COMMAND = get_command("STOP_COMMAND")
@@ -31,7 +31,7 @@ STOP_COMMAND = get_command("STOP_COMMAND")
 async def stop_music(cli, message: Message, _, chat_id):
     if not len(message.command) == 1:
         return await message.reply_text(_["general_2"])
-    await Yukki.stop_stream(chat_id)
+    await Dz.stop_stream(chat_id)
     await set_loop(chat_id, 0)
     await message.reply_text(
         _["admin_9"])
