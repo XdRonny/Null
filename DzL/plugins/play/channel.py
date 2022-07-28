b4b24ff81a832842044a4ea75fbe0994f4217084
DzLyz <86110711+itsdzl@users.noreply.github.com>
2022-07-28 16:21:53 +0700

    Update channel.py

--- DzL/plugins/play/channel.py
+++ DzL/plugins/play/channel.py
@@ -12,9 +12,9 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import get_command
-from YukkiMusic import app
-from YukkiMusic.utils.database import set_cmode
-from YukkiMusic.utils.decorators.admins import AdminActual
+from DzL import app
+from DzL.utils.database import set_cmode
+from DzL.utils.decorators.admins import AdminActual
 
 ### Multi-Lang Commands
 CHANNELPLAY_COMMAND = get_command("CHANNELPLAY_COMMAND")
