fc5975d0d3c35f294da00b583b8204020b39c34d
DzLyz <86110711+itsdzl@users.noreply.github.com>
2022-07-28 16:09:18 +0700

    Update resume.py

--- DzL/plugins/admins/resume.py
+++ DzL/plugins/admins/resume.py
@@ -12,10 +12,10 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import get_command
-from YukkiMusic import app
-from YukkiMusic.core.call import Yukki
-from YukkiMusic.utils.database import is_music_playing, music_on
-from YukkiMusic.utils.decorators import AdminRightsCheck
+from DzL import app
+from DzL.core.call import Dz
+from DzL.utils.database import is_music_playing, music_on
+from DzL.utils.decorators import AdminRightsCheck
 
 # Commands
 RESUME_COMMAND = get_command("RESUME_COMMAND")
@@ -34,6 +34,6 @@ async def resume_com(cli, message: Message, _, chat_id):
     if await is_music_playing(chat_id):
         return await message.reply_text(_["admin_3"])
     await music_on(chat_id)
-    await Yukki.resume_stream(chat_id)
+    await Dz.resume_stream(chat_id)
     await message.reply_text(
         _["admin_4"])
