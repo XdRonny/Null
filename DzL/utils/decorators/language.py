f6a876e223a567739d8e5448b3915fbb2b90cb6b
DzLyz <86110711+itsdzl@users.noreply.github.com>
2022-07-28 17:05:45 +0700

    Update language.py

--- DzL/utils/decorators/language.py
+++ DzL/utils/decorators/language.py
@@ -9,8 +9,8 @@
 
 
 from strings import get_string
-from YukkiMusic.misc import SUDOERS
-from YukkiMusic.utils.database import (get_lang, is_commanddelete_on,
+from DzL.misc import SUDOERS
+from DzL.utils.database import (get_lang, is_commanddelete_on,
                                        is_maintenance)
 
 
