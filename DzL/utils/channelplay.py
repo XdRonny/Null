81f9d6b20cd0fdc0b55a06dba670b5761ec6e0b0
DzLyz <86110711+itsdzl@users.noreply.github.com>
2022-07-28 17:13:24 +0700

    Update channelplay.py

--- DzL/utils/channelplay.py
+++ DzL/utils/channelplay.py
@@ -7,8 +7,8 @@
 #
 # All rights reserved.
 
-from YukkiMusic import app
-from YukkiMusic.utils.database import get_cmode
+from DzL import app
+from DzL.utils.database import get_cmode
 
 
 async def get_channeplayCB(_, command, CallbackQuery):
