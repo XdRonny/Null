3187b4f6027d27fbb163da3ac49161cbdd3f50c3
DzLyz <86110711+itsdzl@users.noreply.github.com>
2022-07-28 15:56:43 +0700

    Update auth.py

--- DzL/plugins/admins/auth.py
+++ DzL/plugins/admins/auth.py
@@ -12,12 +12,12 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS, adminlist
 from strings import get_command
-from YukkiMusic import app
-from YukkiMusic.utils.database import (delete_authuser, get_authuser,
+from DzL import app
+from DzL.utils.database import (delete_authuser, get_authuser,
                                        get_authuser_names,
                                        save_authuser)
-from YukkiMusic.utils.decorators import AdminActual, language
-from YukkiMusic.utils.formatters import int_to_alpha
+from DzL.utils.decorators import AdminActual, language
+from DzL.utils.formatters import int_to_alpha
 
 # Command
 AUTH_COMMAND = get_command("AUTH_COMMAND")
