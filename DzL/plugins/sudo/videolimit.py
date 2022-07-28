96633d119536b07dd1e311b927aa5d9e45c15e80
DzLyz <86110711+itsdzl@users.noreply.github.com>
2022-07-28 16:40:18 +0700

    Update videolimit.py

--- DzL/plugins/sudo/videolimit.py
+++ DzL/plugins/sudo/videolimit.py
@@ -11,10 +11,10 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from strings import get_command
-from YukkiMusic import app
-from YukkiMusic.misc import SUDOERS
-from YukkiMusic.utils.database import set_video_limit
-from YukkiMusic.utils.decorators.language import language
+from DzL import app
+from DzL.misc import SUDOERS
+from DzL.utils.database import set_video_limit
+from DzL.utils.decorators.language import language
 
 VIDEOLIMIT_COMMAND = get_command("VIDEOLIMIT_COMMAND")
 
