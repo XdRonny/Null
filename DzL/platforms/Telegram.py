e295cad244185f1052842c558d2bbee30ae185c0
DzLyz <86110711+itsdzl@users.noreply.github.com>
2022-07-28 19:45:01 +0700

    Update Telegram.py

--- DzL/platforms/Telegram.py
+++ DzL/platforms/Telegram.py
@@ -18,7 +18,7 @@ from pyrogram.types import (InlineKeyboardButton,
 
 import config
 from config import MUSIC_BOT_NAME, lyrical
-from YukkiMusic import app
+from DzL import app
 
 from ..utils.formatters import (convert_bytes, get_readable_time,
                                 seconds_to_min)
