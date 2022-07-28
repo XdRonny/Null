0bca9081804731d10d084f7281d836256d882db6
DzLyz <86110711+itsdzl@users.noreply.github.com>
2022-07-28 16:40:46 +0700

    Update videomode.py

--- DzL/plugins/sudo/videomode.py
+++ DzL/plugins/sudo/videomode.py
@@ -12,10 +12,10 @@ from pyrogram.types import Message
 
 import config
 from strings import get_command
-from YukkiMusic import app
-from YukkiMusic.misc import SUDOERS
-from YukkiMusic.utils.database import add_off, add_on
-from YukkiMusic.utils.decorators.language import language
+from DzL import app
+from DzL.misc import SUDOERS
+from DzL.utils.database import add_off, add_on
+from DzL.utils.decorators.language import language
 
 # Commands
 VIDEOMODE_COMMAND = get_command("VIDEOMODE_COMMAND")
