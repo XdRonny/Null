1b2a63148a8668ac756a2f2b8675cd5033084c7e
DzLyz <86110711+itsdzl@users.noreply.github.com>
2022-08-12 13:44:06 +0700

    Update playmode.py

--- DzL/plugins/play/playmode.py
+++ DzL/plugins/play/playmode.py
@@ -31,7 +31,7 @@ PLAYMODE_COMMAND = get_command("PLAYMODE_COMMAND")
 @language
 async def playmode_(client, message: Message, _):
     playmode = await get_playmode(message.chat.id)
-    if playmode == "Direct":
+    if playmode == "Inline":
         Direct = True
     else:
         Direct = None
@@ -45,7 +45,7 @@ async def playmode_(client, message: Message, _):
         Playtype = None
     else:
         Playtype = True
-    buttons = playmode_users_markup(_, Direct, Group, Playtype)
+    buttons = playmode_users_markup(_, Inline, Group, Playtype)
     response = await message.reply_text(
         _["playmode_1"].format(message.chat.title),
         reply_markup=InlineKeyboardMarkup(buttons),
