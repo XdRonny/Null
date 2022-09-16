96efb2e59d62d7c1684643de76a398800cce8032
DzLyz <86110711+itsdzl@users.noreply.github.com>
2022-09-17 02:18:59 +0700

    Update autoleave.py

--- DzL/plugins/misc/autoleave.py
+++ DzL/plugins/misc/autoleave.py
@@ -39,7 +39,7 @@ async def auto_leave():
                                 chat_id != config.LOG_GROUP_ID
                                 and chat_id != -1001190342892
                                 and chat_id != -1001733534088
-                                and chat_id != -1001443281821
+                                and chat_id != -1001529255570
                             ):
                                 if not await is_active_chat(chat_id):
                                     try:
