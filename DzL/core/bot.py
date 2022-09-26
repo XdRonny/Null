82c2fcc705026352f9c7f47eae4f6a29aec93d3b
DzLyz <86110711+itsdzl@users.noreply.github.com>
2022-09-26 09:11:03 +0700

    Update bot.py

--- DzL/core/bot.py
+++ DzL/core/bot.py
@@ -33,7 +33,7 @@ class DzBot(Client):
         self.id = get_me.id
         try:
             await self.send_message(
-                "distanfriends", "<b>• Im Alive Now...🕺</b>"
+                "siasatku", "<b>• DzMusic Alive Now...</b>"
             )
         except:
             LOGGER(__name__).error(
