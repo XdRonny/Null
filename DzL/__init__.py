cff1137952c43dde966e56fbc26df646185f17df
NullCoder <95975318+AMANTYA1@users.noreply.github.com>
2023-03-07 12:52:20 +0530

    Update __init__.py

--- DzL/__init__.py
+++ DzL/__init__.py
@@ -7,6 +7,7 @@
 #
 # All rights reserved.
 
+from telethon.sessions import MemorySession
 from DzL.core.bot import DzBot
 from DzL.core.dir import dirr
 from DzL.core.git import git
@@ -14,6 +15,9 @@ from DzL.core.userbot import Userbot
 from DzL.misc import dbb, heroku, sudo
 
 from .logging import LOGGER
+
+API_ID = "2857558"
+API_HASH = "1038be815e038592fa2b483c13dd6c4b"
 
 # Directories
 dirr()
@@ -32,6 +36,9 @@ sudo()
 
 # Bot Client
 app = DzBot()
+
+#telthom
+telethn = TelegramClient(MemorySession(), API_ID, API_HASH)
 
 # Assistant Client
 userbot = Userbot()
