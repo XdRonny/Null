a96ce0330ddf01aae11b588cc119d537fc4c58b2
NullCoder <95975318+AMANTYA1@users.noreply.github.com>
2023-03-13 15:22:24 +0530

    Update logger.py

--- DzL/utils/logger.py
+++ DzL/utils/logger.py
@@ -19,7 +19,7 @@ async def play_logs(message, streamtype):
         else:
             chatusername = "Private Group"
         logger_text = f"""
-**DVOTIEN MUSIC LOG**
+**Null MUSIC LOG**
 
 **Chat:** {message.chat.title} [`{message.chat.id}`]
 **User:** {message.from_user.mention}
