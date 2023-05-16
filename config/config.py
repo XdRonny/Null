c77ffadf6461163acf9c8c2ea40f8e7d18d0593d
XdRonny <127653254+XdRonny@users.noreply.github.com>
2023-05-16 16:36:53 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -56,7 +56,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/AMANTYA1/Bug",
+    "https://github.com/XdRonny/Null",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 
